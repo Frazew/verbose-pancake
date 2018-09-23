d722384bf43d4bed606409d0ca807f4402c3b826
Frazew <frazewmc@gmail.com>
2018-09-23 22:27:59 +0200

    Ajout d'un moyen d'afficher des commandes inline de manière propre

--- .tools/genpdf.sh
+++ .tools/genpdf.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 wname=$(basename $(pwd))
-pandoc $wname.md --template $TOOLS_PATH/eisvogel.tex --latex-engine=xelatex -o $wname.pdf --listings -colorlinks -V lang=fr-FR
+
+pandoc $wname.md --template $TOOLS_PATH/eisvogel.tex --pdf-engine=xelatex -o $wname.tex -colorlinks -V lang=fr-FR --listings
+sed -i -E 's/\\lstinline\[language=bash\].(.*?).\}/\\shellcmd{\1}}/' $wname.tex
+
+xelatex $wname.tex 1> /dev/null
