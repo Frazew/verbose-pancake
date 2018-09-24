4dbfcf6fccd1aa2bfddaf4a0a31f94ac9eea9752
Frazew <frazewmc@gmail.com>
2018-09-24 19:23:26 +0200

    Detection de pandoc>2 pour pdf/latex-engine

--- .tools/genpdf.sh
+++ .tools/genpdf.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 wname=$(basename $(pwd))
 
-pandoc $wname.md --template $TOOLS_PATH/eisvogel.tex --pdf-engine=xelatex -o $wname.tex -colorlinks -V lang=fr-FR --listings
+TEX_ARG="--latex-engine"
+if pandoc -v | grep "2\.." >/dev/null; then TEX_ARG="--pdf-engine"; fi
+pandoc $wname.md --template $TOOLS_PATH/eisvogel.tex $TEX_ARG=xelatex -o $wname.tex -colorlinks -V lang=fr-FR --listings
 sed -i -E 's/\\lstinline\[language=bash\].(.*?).\}/\\shellcmd{\1}}/' $wname.tex
 
 xelatex $wname.tex 1> /dev/null
