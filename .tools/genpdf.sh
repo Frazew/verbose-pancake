97646a5eaa62564cfcadeb10daf71389513b411f
Frazew <frazewmc@gmail.com>
2018-09-24 22:00:10 +0200

    Ajout du gitignore et correction de la fonte après un listing inline

--- .tools/genpdf.sh
+++ .tools/genpdf.sh
@@ -4,6 +4,6 @@ wname=$(basename $(pwd))
 TEX_ARG="--latex-engine"
 if pandoc -v | grep "2\.." >/dev/null; then TEX_ARG="--pdf-engine"; fi
 pandoc $wname.md --template $TOOLS_PATH/eisvogel.tex $TEX_ARG=xelatex -o $wname.tex -colorlinks -V lang=fr-FR --listings
-sed -i -E 's/\\lstinline\[language=bash\].(.*?).\}/\\shellcmd{\1}}/' $wname.tex
+sed -i -E 's/\\passthrough\{\\lstinline\[language=bash\].(.*?).\}/{\\shellcmd{\1}}/' $wname.tex
 
 xelatex $wname.tex 1> /dev/null
